f41895272c9ed8e2f582a6059ec3f4db1e2fc102
JoelRuegger <rueggerjoel@gmail.com>
2023-04-12 10:11:20 +0200

    Started infinite list scrolling component.

---
+++ src/components/Loading.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Loading = () => {
+    return(
+        <div className="container-loading">
+            <div className="spinner"></div>
+            <span>Loading more list items...</span>
+        </div>
+    );
+}
